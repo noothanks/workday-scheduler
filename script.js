0b8e5b45977cdc04a3227177c38197012897cec3
noothanks <coreyfuller727@yahoo.com>
2022-09-09 15:25:22 -0400

    setting up localStorage functionality and fixing moment.js issue

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ var currentDayEl = $("#currentDay");
 
 var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var hourArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var appointmentArr = [];
+
+var hour9 = $('#9');
+var hour10 = $('#10');
+var hour11 = $('#11');
+var hour12 = $('#12');
+var hour13 = $('#13');
+var hour14 = $('#14');
+var hour15 = $('#15');
+var hour16 = $('#16');
+var hour17 = $('#17');
 
 var displayDate = function () {
     //get current month
@@ -23,41 +32,84 @@ var displayDate = function () {
 }
 
 var colorCode = function() {
-    var hour = moment().hour();
 
-    for(i=0; i< hourArr.length; i++) {
+    for(i=0; i< 9; i++) {
+        var hour = moment().hour();
         if(hour < hourArr[i]) {
-            var hourId = hourArr[i].toString();
+            var hourId = hourArr[i];
             $(`#${hourId}`).addClass('future');
         }
 
-        if (hour = hourArr[i]) {
+        if (hour === hourArr[i]) {
             var hourId = hour.toString();
             $(`#${hourId}`).addClass('present');
-            // return;
         }     
     }
 }
 
-var handleChange = function(event) {
-    var hourId = event.target.id;
-    var appointmentText = event.target.value
-    return appointmentText, hourId
-}
+// var handleChange = function(event) {
+//     var hourId = event.target.id;
+//     var appointmentText = event.target.value
+//     return appointmentText, hourId
+// }
 
 var saveAppointments = function() {
-    for (i=0; i<hourArr; i++) {
-        //get hour id
-        //get appointmentText
-        //create an object for each hour
-        //set hour object property to corresponding textArea id
-        //set appointment object property to corresponding textArea value
-        //stringify and set to local storage
-    }
+
+    var appointmentArr = [
+        {
+            id: 9,
+            text: hour9.val()
+        },
+        {
+            id: 10,
+            text: hour10.val()
+        },
+        {
+            id: 11,
+            text: hour11.val()
+        },
+        {
+            id: 12,
+            text: hour12.val()
+        },
+        {
+            id: 13,
+            text: hour13.val()
+        },
+        {
+            id: 14,
+            text: hour14.val()
+        },
+        {
+            id: 15,
+            text: hour15.val()
+        },
+        {
+            id: 16,
+            text: hour16.val()
+        },
+        {
+            id: 17,
+            text: hour17.val()
+        }
+    ];
+
+    localStorage.setItem("appointments", JSON.stringify(appointmentArr));
+
+    window.location.reload();
+}
+
+var loadAppointments = function() {
+     var db = JSON.parse(localStorage.getItem("appointments")) || []
+
+     for(i=0; i<db.length; i++) {
+        $(`#${i + 9}`).val(db[i].text)
+     }
 }
 
 displayDate();
 colorCode();
+loadAppointments();
 
-$("textarea").on('input', handleChange);
+// $("textarea").on('input', handleChange);
 saveBtnEl.on('click', saveAppointments);
